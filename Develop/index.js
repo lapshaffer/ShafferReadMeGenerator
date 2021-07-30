8e2c67b6b527a4757f14f5f11a88f3fbb5a6459b
lapshaffer <lapshaffer@gmail.com>
2021-07-30 14:36:18 -0400

    set up const to call inquirer and fs into js file, began writing prompts and function to write ReadMe file

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,35 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+           type: 'input',
+           message: 'What is the title of your project?',
+           name: 'title' 
+        },
+        {
+            type: 'input',
+            message: 'Please provide a description of your project',
+            name: 'description'
+        }
+    ])
+    .then((response) => {
+        console.log(response);
+        // TODO: Create a function to write README file
+        const inputs = `This is where the ReadMe markdown will go, once I write it`
+        fs.writeFile('README.md', inputs, (error, data) =>
+        error ? console.error(error) : console.log(data)
+      );
+    })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {},
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {},
 
 // Function call to initialize app
-init();
+// init();
